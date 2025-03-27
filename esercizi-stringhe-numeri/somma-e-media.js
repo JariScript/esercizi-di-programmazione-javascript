275155a036f1e90a5ed16e0a94670d53332b7a51
JariScript <jari.margiotta@gmail.com>
2025-03-27 11:10:26 +0100

    Update somma-e-media.js

--- esercizi-stringhe-numeri/somma-e-media.js
+++ esercizi-stringhe-numeri/somma-e-media.js
@@ -8,3 +8,17 @@
   
   http://www.imparareaprogrammare.it
 */
+
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+let e = 5;
+
+let sum = a + b + c + d + e;
+let average = (a + b + c + d + e)/5;
+
+console.log(sum);
+console.log(average);
+
+console.log(`La somma tra gli elementi è ${sum} e la loro media è ${average}`);
