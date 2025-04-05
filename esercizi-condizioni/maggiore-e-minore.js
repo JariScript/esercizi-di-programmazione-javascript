a5ab255e8be5768f8912f3cff4dfb95d4e68ba92
JariScript <jari.margiotta@gmail.com>
2025-04-05 16:53:51 +0200

    Update maggiore-e-minore.js

--- esercizi-condizioni/maggiore-e-minore.js
+++ esercizi-condizioni/maggiore-e-minore.js
@@ -10,3 +10,28 @@
 
   http://www.imparareaprogrammare.it
 */
+
+let a = 3;
+let b = -1;
+let c = 4;
+let d = -2;
+
+if (a > b && a > c && a > d) {
+  console.log("Maggiore: " + a);
+} else if (b > a && b > c && b > d) {
+  console.log("Maggiore: " + b);
+} else if (c > a && c > b && c > d) {
+  console.log("Maggiore: " + c);
+} else if (d > a && d > b && d > c) {
+  console.log("Maggiore: " + d);
+}
+
+if (a < b && a < c && a < d) {
+  console.log("Minore: " + a);
+} else if (b < a && b < c && b < d) {
+  console.log("Minore: " + b);
+} else if (c < a && c < b && c < d) {
+  console.log("Minore: " + c);
+} else if (d < a && d < b && d < c) {
+  console.log("Minore: " + d);
+}
