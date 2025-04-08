0c5bf087c45bc17e590742124f930795f3116a19
JariScript <jari.margiotta@gmail.com>
2025-04-08 09:14:12 +0200

    Update menu-dei-dolci.js

--- esercizi-condizioni/menu-dei-dolci.js
+++ esercizi-condizioni/menu-dei-dolci.js
@@ -21,3 +21,53 @@
 
   http://www.imparareaprogrammare.it
 */
+
+// Definizione del menu come oggetto
+const menu = {
+  1: 'Tiramisù',
+  2: 'Torta della Nonna',
+  3: 'Cheesecake',
+  4: 'Macedonia'
+};
+
+// Input dell'utente
+let input = 3;
+
+// Controllo della scelta
+if (menu[input]) {
+  console.log(`Hai scelto il dolce ${menu[input]}`);
+} else {
+  console.log('Dolce non disponibile');
+}
+
+
+
+
+//Altro metodo però meno efficiente 
+let tiramisu = 1;
+let tortadellanonna = 2;
+let cheescake = 3;
+let macedonia = 4;
+
+let input1 = 3;
+if (input1 == tiramisu) {
+  console.log('Hai scelto il dolce Tiramisù');
+}
+ else if (input1 == tortadellanonna) {
+  console.log('Hai scelto il dolce Torta della Nonna');
+}
+
+else if (input1 == cheescake) {
+  console.log('Hai scelto il dolce Cheesecake');
+}
+
+else if (input1 == macedonia)  {
+  console.log(`Hai scelto il dolce Macedonia`);
+}
+
+else {
+  console.log('Dolce non disponibile');
+}
+ 
+ 
+
